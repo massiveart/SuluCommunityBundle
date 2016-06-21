d4301646798c62f9ee0275a5f72066fbe5802e59
Alexander Schranz <alexander.schranz@massiveart.com>
2016-06-21 13:36:52 +0200

    register completion listener in service xml and set or unset in compilerpass

--- DependencyInjection/CompilerPass/CommunityValidatorCompilerPass.php
+++ DependencyInjection/CompilerPass/CommunityValidatorCompilerPass.php
@@ -22,11 +22,19 @@ use Symfony\Component\DependencyInjection\Reference;
  */
 class CommunityValidatorCompilerPass implements CompilerPassInterface
 {
+    const COMPLETION_LISTENER_SERVICE_ID = 'sulu_community.completion_listener';
+
     /**
      * {@inheritdoc}
      */
     public function process(ContainerBuilder $container)
     {
+        // If no listener exists do nothing
+        if (!$container->hasDefinition(self::COMPLETION_LISTENER_SERVICE_ID)) {
+            return;
+        }
+
+        // Create Validator References
         $config = $container->getParameter('sulu_community.config');
 
         foreach ($config[Configuration::WEBSPACES] as $webspaceKey => $webspaceConfig) {
@@ -40,13 +48,10 @@ class CommunityValidatorCompilerPass implements CompilerPassInterface
 
         // Register request listener only when validator exists.
         if (!empty($validators)) {
-            $definition = new DefinitionDecorator('sulu_community.completion_listener.abstract');
+            $definition = $container->getDefinition(self::COMPLETION_LISTENER_SERVICE_ID);
             $definition->replaceArgument(3, $validators);
-            $definition->addTag('kernel.event_listener', ['event' => 'kernel.request', 'method' => 'onRequest']);
-            $container->setDefinition(
-                sprintf('sulu_community.completion_listener', $validators),
-                $definition
-            );
+        } else {
+            $container->removeDefinition(self::COMPLETION_LISTENER_SERVICE_ID);
         }
     }
 }
